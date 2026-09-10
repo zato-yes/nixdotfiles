e1ee184da51926b6ca494691bf1c0bb0d27ff3fa
zato-yes <zatoyes@duck.com>
2026-09-11 00:26:58 +0200

    add drivers for audio, ethernet, usb storage

--- nixModules/kernelDriverConfiguration.nix
+++ nixModules/kernelDriverConfiguration.nix
@@ -17,6 +17,15 @@ boot.initrd.kernelModules = [ ];
 boot.kernelModules = [ 
 	"kvm-intel"
 	"usb_storage"
+	"uas"
+	"sd_mod"
+	"snd_usb_audio"
+	"snd_ump"
+	"cdc_ether"
+	"usbnet"
+	"mii"
+	"r8152"
+
 
 
 ];
